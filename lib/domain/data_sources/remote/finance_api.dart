ca1cee37fced6f7f379fcba5bd1b8ba18da4aea2
Amir-HShahi <amir13831107h@gmail.com>
2025-03-28 20:32:48 +0330

    abstract remote api

--- lib/domain/data_sources/remote/finance_api.dart
+++ lib/domain/data_sources/remote/finance_api.dart
@@ -1,3 +1,24 @@
-abstract class FinanceApi {
+import 'dart:async';
 
+import 'package:financial/domain/entities/category.dart';
+import 'package:financial/domain/entities/transaction.dart';
+
+abstract class FinanceApi {
+  Future<List<Transaction>> getAllTransactions();
+
+  Future<void> postTransaction(Transaction transaction);
+
+  Future<Transaction> getTransaction(int id);
+
+  Future<void> deleteTransaction(int id);
+
+  Future<void> updateTransaction(Transaction transaction);
+
+  Future<void> increaseBudget(Category category);
+
+  Future<void> decreaseBudget(Category category);
+
+  Future<List<Category>> getAllCategories();
+
+  Future<Category> getCategory(int id);
 }
